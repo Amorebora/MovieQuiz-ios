65e4c81ab8741ab6261344e0f62a7d5c9a864332
Maksim Baev <maximbaeves@gmail.com>
2022-09-14 18:08:15 +0200

    Add UI tests for buttons and alert

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -8,34 +8,86 @@
 import XCTest
 
 class MovieQuizUITests: XCTestCase {
-
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
+        app = XCUIApplication()
+       app.launch()
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
+        app.terminate()
+        app = nil
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+   
+   func testYesButton() {
+       let firstPoster = app.images["Poster"]
+       
+       app.buttons["Yes"].tap()
+       
+      sleep(3)
+      
+       let secondPoster = app.images["Poster"]
+       let indexLabel = app.staticTexts["Index"]
+       
+       
+       XCTAssertTrue(indexLabel.label == "2/10")
+       XCTAssertFalse(firstPoster == secondPoster)
+   }
+   
+   func testNoButton() {
+       let firstPoster = app.images["Poster"]
+       
+       app.buttons["No"].tap()
+       
+      sleep(3)
+      
+       let secondPoster = app.images["Poster"]
+       let indexLabel = app.staticTexts["Index"]
+       
+       
+       XCTAssertTrue(indexLabel.label == "2/10")
+       XCTAssertFalse(firstPoster == secondPoster)
+   }
+   
+   func testResultAlert() {
+       
+      for index in 1...9 {
+         let indexLabel = app.staticTexts["Index"]
+         XCTAssertTrue(indexLabel.label == "\(index)/10")
+         
+         let firstPoster = app.images["Poster"]
+         app.buttons["Yes"].tap()
+          
+         sleep(5)
+         
+         let secondPoster = app.images["Poster"]
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+         XCTAssertFalse(firstPoster == secondPoster)
+      }
+      
+      app.buttons["Yes"].tap()
+      sleep(3)
+      let indexLabel = app.staticTexts["Index"]
+      XCTAssertTrue(indexLabel.label == "10/10")
+      
+      let alert = app.alerts.firstMatch
+      XCTAssertTrue(alert.exists)
+      
+      let continueButton = alert.buttons["continueGame"]
+      XCTAssertEqual(continueButton.label, "Сыграть ещё раз")
+      continueButton.tap()
+      
+      sleep(3)
+      
+      let newIndexLabel = app.staticTexts["Index"]
+      XCTAssertTrue(newIndexLabel.label == "1/10")
+   }
 }
